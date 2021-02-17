0a9ad9ba9002bddd724423f24cfa9d54c596c304
Володкин Дмитрий <volodkin840@gmail.com>
2021-02-17 21:49:43 +0300

    Добавьте файлы проекта.

---
+++ Экзамен/MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace Экзамен
+{
+    
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Point p;
+        int maxz;
+        Size s;
+        public Point A;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            A_X.Maximum = (int)Container.Width;
+            A_X.Minimum = 0;
+            A_Y.Maximum = (int)Container.Height;
+            A_Y.Minimum = 0;
+            Height.Minimum = 0;
+            Width.Minimum = 0;
+            Length.Minimum = 0;
+            Height.Maximum = (int)Container.Height;
+            Width.Maximum = (int)Container.Width; ;
+            Length.Maximum = (int)Container.Height;
+            Thickness.Maximum = 10;
+            Thickness.Minimum = 1;
+            buttoncreate.Focus();
+            A = new Point(100, 100);
+            
+        }       
+
+        private void clear_click(object sender, RoutedEventArgs e)
+        {
+            Container.Children.Clear();
+        }
+        private void Create_click_but(object sender, RoutedEventArgs e)
+        {
+            if (!(A_X.Text == null ^ A_Y.Text == null ^ Height.Text == null ^ Length.Text == null ^ Width.Text == null ^ Thickness.Text == null))
+            {
+                try
+                {
+                    var converter = new System.Windows.Media.BrushConverter();
+                    Container.Children.Clear();
+
+                    if (Convert.ToBoolean(Checke.IsChecked))
+                    {
+                        Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToBoolean(Checke.IsChecked));
+                    }
+                    else
+                    {
+                        Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString(colorlines2.SelectedColorText));
+                    }
+                }
+                catch
+                {
+                    
+                }
+            }
+            }
+        private void Create_click(object sender, RoutedEventArgs e)
+        {
+            if (!(A_X.Text == null & A_Y.Text == null & Height.Text == null & Length.Text == null & Width.Text == null & Thickness.Text == null))
+            {
+                try
+                {
+                    var converter = new System.Windows.Media.BrushConverter();
+                Container.Children.Clear();
+
+
+                if (Convert.ToBoolean(Checke.IsChecked))
+                {
+                    Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToBoolean(Checke.IsChecked));
+                }
+                else
+                {
+                    Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString(colorlines.SelectedColorText), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString(colorlines2.SelectedColorText));
+                }
+                A_X.Text = Convert.ToString(A.X);
+                A_Y.Text = Convert.ToString(A.Y);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            A = Mouse.GetPosition(this);
+            Create_click(sender, e);
+        }
+
+        private void Double_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(Char.IsDigit(e.Text, 0)))
+            {
+                
+                e.Handled = true;
+               
+            }
+        }
+        private void KeyEvents(object sender, KeyEventArgs e)
+        {
+            var converter = new System.Windows.Media.BrushConverter();
+            switch (e.Key)
+            {
+                case Key.Up:
+                    A.Y--;
+                    Create_click(sender,e); break;
+                case Key.Down:
+                    A.Y++;
+                    Create_click(sender, e); break; 
+                case Key.Left:
+                    A.X--;
+                    Create_click(sender, e); break; 
+                case Key.Right:
+                    A.X++;
+                    Create_click(sender, e); break; 
+            }
+        }
+        private void canvas1_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+
+            if (e.Source == Container)
+            {
+                Title = "Mouse";
+                return;
+            }
+
+            var a = e.Source as FrameworkElement;
+            Point q = e.GetPosition(a);
+
+            Canvas.SetZIndex(a, ++maxz);
+
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Canvas.SetLeft(a, Math.Max(0, Canvas.GetLeft(a) + q.X - p.X));
+                Canvas.SetTop(a, Math.Max(0, Canvas.GetTop(a) + q.Y - p.Y));
+            }
+            else if (e.RightButton == MouseButtonState.Pressed)
+            {
+                a.Width = Math.Max(20, s.Width + q.X - p.X);
+                a.Height = Math.Max(20, s.Height + q.Y - p.Y);
+            }
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            ColorPicker colorPicker = new ColorPicker();
+            var converter = new System.Windows.Media.BrushConverter();
+            A = new Point(50, 75);
+            Height.Text = Convert.ToString(100);
+            Width.Text = Convert.ToString(50);
+            Length.Text = Convert.ToString(150);
+            Container.Children.Clear();
+
+
+
+                Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString("#FF0000"), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString("#8B0000"), true);
+            
+
+            
+            A_X.Text = Convert.ToString(A.X);
+            A_Y.Text = Convert.ToString(A.Y);
+
+
+        }
+
+        private void Button_Click2(object sender, RoutedEventArgs e)
+        {
+            ColorPicker colorPicker = new ColorPicker();
+            var converter = new System.Windows.Media.BrushConverter();
+            A = new Point(100, 150);
+            Height.Text = Convert.ToString(50);
+            Width.Text = Convert.ToString(75);
+            Length.Text = Convert.ToString(30);
+            
+            Container.Children.Clear();
+
+                Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString("#FF0000"), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString("#8B0000"),2);
+            
+            A_X.Text = Convert.ToString(A.X);
+            A_Y.Text = Convert.ToString(A.Y);
+        }
+
+        private void Button_Click3(object sender, RoutedEventArgs e)
+        {
+            ColorPicker colorPicker = new ColorPicker();
+            var converter = new System.Windows.Media.BrushConverter();
+            A = new Point(500, 500);
+            Height.Text = Convert.ToString(100);
+            Width.Text = Convert.ToString(50);
+            Length.Text = Convert.ToString(150);
+            Container.Children.Clear();
+
+
+
+            Parallepiped newf = new Parallepiped(A, Convert.ToInt32(Height.Text), Convert.ToInt32(Length.Text), Convert.ToInt32(Width.Text), Container, (Brush)converter.ConvertFromString("#FF0000"), Convert.ToInt32(Thickness.Text), (Brush)converter.ConvertFromString("#8B0000"), Convert.ToBoolean(Checke.IsChecked));
+
+
+
+            A_X.Text = Convert.ToString(A.X);
+            A_Y.Text = Convert.ToString(A.Y);
+        }
+    }
+}
---
+++ Экзамен/Parallepiped.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Экзамен
+{
+
+   public class Parallepiped
+    {
+        Point A;
+        Point A1;
+        Point B;
+        Point B1;
+        Point C;
+        Point C1;
+        Point D;
+        Point D1;
+
+        public Parallepiped(Point A, int heigth, int length, int width, Canvas canvas, Brush color1, int Thickness, Brush color2,bool checke,int checkedtest=0)
+        {
+            double x = A.X;
+            double y = A.Y;
+            this.A = A;
+            B.X = x + Math.Cos(Math.PI / 4) + width;
+            B.Y = y + Math.Sin(Math.PI / 4) - width;
+            C.X = x + length + Math.Cos(Math.PI / 4) + width;
+            C.Y = y + Math.Sin(Math.PI / 4) - width;
+            D.X = x + length;
+            D.Y = y;
+            A1.X = x;
+            A1.Y = y + heigth;
+            B1.X = x + Math.Cos(Math.PI / 4) + width;
+            B1.Y = y + heigth + Math.Sin(Math.PI / 4) - width;
+            C1.X = x + length + Math.Cos(Math.PI / 4) + width;
+            C1.Y = y + heigth + Math.Sin(Math.PI / 4) - width;
+            D1.X = x + length;
+            D1.Y = y + heigth;
+
+            Label lable1 = new Label();
+            lable1.Content = "Фигура выходит за пределы контейнера";
+
+            if (Check(canvas, A) & Check(canvas, B) & Check(canvas, C) & Check(canvas, D) & Check(canvas, A1) & Check(canvas, B1) & Check(canvas, C1) & Check(canvas, D1))
+            {
+                if (checkedtest==2)
+                {
+                     if (!checke) create2_random(canvas, color2);
+                    create(canvas, color1, Thickness, checke);
+                }
+                else if (!checke) create2(canvas, color2);
+                create(canvas, color1, Thickness,checke);                
+            }
+            else
+            {
+                canvas.Children.Add(lable1);
+            }
+            
+        }
+        public Parallepiped(Point A, int heigth, int length, int width, Canvas canvas, Brush color1, int Thickness, Brush color2,int checkedtest = 0)
+        {
+            double x = A.X;
+            double y = A.Y;
+            this.A = A;
+            B.X = x + Math.Cos(Math.PI / 4) + width;
+            B.Y = y + Math.Sin(Math.PI / 4) - width;
+            C.X = x + length + Math.Cos(Math.PI / 4) + width;
+            C.Y = y + Math.Sin(Math.PI / 4) - width;
+            D.X = x + length;
+            D.Y = y;
+            A1.X = x;
+            A1.Y = y + heigth;
+            C1.X = x + length + Math.Cos(Math.PI / 4) + width;
+            C1.Y = y + heigth + Math.Sin(Math.PI / 4) - width;
+            D1.X = x + length;
+            D1.Y = y + heigth;
+
+            Label lable1 = new Label();
+            lable1.Content = "Фигура выходит за пределы контейнера";
+
+            if (Check(canvas, A) & Check(canvas, B) & Check(canvas, C) & Check(canvas, D) & Check(canvas, A1) & Check(canvas, B1) & Check(canvas, C1) & Check(canvas, D1))
+            {
+                if (checkedtest == 2)
+                {
+                    if (!false) create2_random(canvas, color2);
+                    create(canvas, color1, Thickness, false);
+                }
+                 else   if (!false) create2(canvas, color2);
+                create(canvas, color1, Thickness, false);
+            }
+            else
+            {
+                canvas.Children.Add(lable1);
+            }
+
+        }
+        void create (Canvas canvas, Brush color1,int Thickness,bool checke)
+        {
+            PointCollection myPointCollection = new PointCollection();
+            PointCollection myPointCollection2 = new PointCollection();
+            Polyline myPolyline = new Polyline();
+            Polyline myPolyline2 = new Polyline();
+            myPointCollection.Add(A);
+            myPointCollection.Add(A1);
+            myPointCollection.Add(D1);
+            myPointCollection.Add(D);
+            myPointCollection.Add(A);
+            myPointCollection.Add(B);
+            myPointCollection.Add(C);
+            myPointCollection.Add(D);
+            myPointCollection.Add(D1);
+            myPointCollection.Add(C1);
+            myPointCollection.Add(C);
+            myPolyline.Points = myPointCollection;
+            myPolyline.Stroke = color1;
+            myPolyline.StrokeThickness = Thickness;           
+            canvas.Children.Add(myPolyline);
+            if (checke)
+                {
+                myPointCollection2.Add(B);
+                myPointCollection2.Add(B1);
+                myPointCollection2.Add(A1);
+                myPointCollection2.Add(B1);
+                myPointCollection2.Add(C1);
+                myPolyline2.Points = myPointCollection2;
+                myPolyline2.Stroke = color1;
+                myPolyline2.StrokeThickness = Thickness;
+                myPolyline2.StrokeDashArray.Add(2);
+                canvas.Children.Add(myPolyline2);
+            }
+        }
+        void create2(Canvas canvas, Brush color2)
+        {
+            Polygon polygon = new Polygon();
+            Polygon polygon2 = new Polygon();
+            Polygon polygon3 = new Polygon();
+            PointCollection points1 = new PointCollection() {A,D,D1,A1};
+            PointCollection points2 = new PointCollection() { A, B, C, D };
+            PointCollection points3 = new PointCollection() { D,C,C1,D1 };
+            polygon.Points = points1;
+            polygon.Fill = color2;
+            polygon2.Points = points2;
+            polygon2.Fill = color2;
+            polygon3.Points = points3;
+            polygon3.Fill = color2;
+            canvas.Children.Add(polygon);
+            canvas.Children.Add(polygon2);
+            canvas.Children.Add(polygon3);
+        }
+        void create2_random(Canvas canvas, Brush color2)
+        {
+            Random r = new Random();
+            var converter = new System.Windows.Media.BrushConverter();
+            Polygon polygon = new Polygon();
+            Polygon polygon2 = new Polygon();
+            Polygon polygon3 = new Polygon();
+            PointCollection points1 = new PointCollection() { A, D, D1, A1 };
+            PointCollection points2 = new PointCollection() { A, B, C, D };
+            PointCollection points3 = new PointCollection() { D, C, C1, D1 };
+            polygon.Points = points1;
+            polygon.Fill = (Brush)converter.ConvertFromString($"#{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}");
+            polygon2.Points = points2;
+            polygon2.Fill = (Brush)converter.ConvertFromString($"#{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}");
+            polygon3.Points = points3;
+            polygon3.Fill = (Brush)converter.ConvertFromString($"#{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}{r.Next(0, 9)}");
+            canvas.Children.Add(polygon);
+            canvas.Children.Add(polygon2);
+            canvas.Children.Add(polygon3);
+        }
+        private bool Check(Canvas container, Point A)
+        {
+            return (A.X >= 0 & A.X < container.Width & A.Y >= 0 & A.Y < container.Height);
+        }
+
+    }
+}
